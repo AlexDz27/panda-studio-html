fe32de389160b5b3b29bff4cb682f4082c439a7b
Alexey Dzyuba <alexeydzyuba27@gmail.com@>
2020-03-19 16:16:44 +0400

    Added article page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log(123);
+// const burger = document.querySelector('.burger');
+
+// burger.addEventListener('click', () => {
+//   burger.classList.toggle('burger__bar--change');
+// });
+
+function HeaderMobileNavManager () {
+	this.burger = document.querySelector('.burger');
+	this.navList = document.querySelector('.nav-list');
+
+	var activateNav = function () {
+		this.burger.classList.toggle('burger__bar--change');
+		this.navList.classList.toggle('nav-list-mobile--active');
+	}.bind(this);
+
+	this.burger.addEventListener('click', activateNav)
+}
+
+var headerMobileNavManager = new HeaderMobileNavManager();
